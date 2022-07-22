f5feb875cc0957c4518f0f3f69b1237931531baf
Administrator <Administrator@SC-202106161522>
2022-07-22 12:26:04 +0900

    응

---
+++ src/capitals/CapitalsDao.java
@@ -0,0 +1,13 @@
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CapitalsDao {
+
+	int create(int number, String question, String answer, String continent) throws SQLException;
+
+	List<Capitals> read() throws SQLException;
+
+	int update(int number, String question, String answer, String continet) throws SQLException;
+
+	int delete(int number) throws SQLException;
+}
---
+++ src/capitals/Test.java
@@ -0,0 +1,44 @@
+import java.sql.SQLException;
+public class Test {
+	private Manager mg;
+
+	public Test(Manager mg) {
+		super();
+		this.mg = mg;
+	}
+
+	public static void main(String[] args) {
+		Manager mg = new Manager();
+		try {
+			mg.create(101, "독일", "베를린", "유럽");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+//	}
+//		try {
+//			mg.read();
+//			System.out.println("보여줘");
+//		} catch (Exception e) {
+//			System.out.println("?");
+//		}
+//		
+//		try {
+//		mg.update(1, "일본", "도쿄", "아시아");
+//		System.out.println("수정완료");
+//		} catch(SQLException e) {
+//			System.out.println("?");
+//		}
+//		
+//		try {
+//			mg.delete(3);
+//			System.out.println("삭제?");
+//		} catch(SQLException e) {
+//			e.printStackTrace();
+//		}
+		
+		
+		
+		}
+
+}
