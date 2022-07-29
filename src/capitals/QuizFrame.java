6e02ecb541fa32204f48b4f9c4f6d094fdb64789
Administrator <Administrator@SC-202106161522>
2022-07-29 16:01:42 +0900

    change

--- src/capitals/QuizFrame.java
+++ src/capitals/QuizFrame.java
@@ -6,11 +6,10 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
